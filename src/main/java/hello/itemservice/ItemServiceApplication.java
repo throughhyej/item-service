ac54e86ae2f2212cd30af066e87e91e9b3fb56f2
throughhyej <hyejeyh@gmail.com>
2022-03-12 10:19:08 +0900

    스프링 MVC2 - validation V1 (Validator 분리 2: 검증기 전역설정 방법 추가)

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,7 +1,10 @@
 package hello.itemservice;
 
+import hello.itemservice.validatior.ItemValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.validation.Validator;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ItemServiceApplication {
@@ -11,3 +14,19 @@ public class ItemServiceApplication {
 	}
 
 }
+
+
+//@SpringBootApplication
+//public class ItemServiceApplication implements WebMvcConfigurer {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(ItemServiceApplication.class, args);
+//	}
+//
+//	// 검증기 전역 적용 (전역으로는 잘 사용하지 않음)
+//	@Override
+//	public Validator getValidator() {
+//		return new ItemValidator();
+//	}
+//
+//}
