c1b8ee1957bfeb78f756b0ccaa130a3160b59937
throughhyej <hyejeyh@gmail.com>
2022-03-16 18:08:14 +0900

    스프링 MVC2 - 로그인 처리 V1 (쿠키 설정: 현업에선 사용하지 않음)

--- src/main/java/hello/itemservice/web/HomeController.java
+++ src/main/java/hello/itemservice/web/HomeController.java
@@ -1,13 +1,34 @@
 package hello.itemservice.web;
 
+import hello.itemservice.domain.member.Member;
+import hello.itemservice.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
+    private final MemberRepository memberRepository;
+
+//    @RequestMapping("/")
+//    public String home() {
+//        return "home";
+//    }
+
     @RequestMapping("/")
-    public String home() {
-        return "home";
+    public String home(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+
+        if (memberId == null) return "home";
+
+        Member member = memberRepository.findById(memberId);
+        if (member == null) return "home";
+
+        model.addAttribute("member", member);
+        return "loginHome";
     }
+
 }
--- src/main/java/hello/itemservice/web/login/LoginController.java
+++ src/main/java/hello/itemservice/web/login/LoginController.java
@@ -2,6 +2,7 @@ package hello.itemservice.web.login;
 
 import hello.itemservice.domain.login.LoginService;
 import hello.itemservice.domain.member.Member;
+import hello.itemservice.domain.member.MemberRepository;
 import hello.itemservice.web.member.LoginForm;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,21 +11,25 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Slf4j
 @Controller
-@RequestMapping("/login")
 @RequiredArgsConstructor
 public class LoginController {
 
     private final LoginService loginService;
+    private static final String COOKIE_NAME = "memberId";
 
-    @GetMapping
+    @GetMapping("/login")
     public String loginForm(@ModelAttribute LoginForm loginForm) {
         return "login/login";
     }
 
-    @PostMapping
-    public String login(@Validated @ModelAttribute LoginForm loginForm, BindingResult bindingResult) {
+    @PostMapping("/login")
+    public String login(@Validated @ModelAttribute LoginForm loginForm, BindingResult bindingResult, HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
             return "/login/login" ;
         }
@@ -36,10 +41,23 @@ public class LoginController {
             return "/login/login" ;
         }
 
-        // TODO:
-
+        // Cookie 이용 (다음의 보안 문제로 사용하지 않음)
+        // 1. 쿠키값은 임의로 변경이 가능하다.
+        // 2. 탈취 가능 (중요한 정보 담지 말기)
+        // => 대안 (토큰: 서버에서 토큰 만료시간 짧게 지정) : session
+        Cookie cookie = new Cookie(COOKIE_NAME, String.valueOf(login.getId()));
+        response.addCookie(cookie);
 
         log.info("### 로그인 성공 ###");
+        return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        Cookie cookie = new Cookie(COOKIE_NAME, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+
         return "redirect:/";
     }
 }
